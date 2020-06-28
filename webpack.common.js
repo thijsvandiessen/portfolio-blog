b03b7f043ce0f6043ba1317362cd684c0683065d
Thijs van Diessen <thijs.vandiessen@incentro.com>
2020-06-28 11:44:36 +0200

    fix copy plugin + added clean plugin

--- webpack.common.js
+++ webpack.common.js
@@ -4,8 +4,8 @@ const webpack = require('webpack');
 // to create dynamicly an html file with the right sources
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// public folder
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 
@@ -20,6 +20,16 @@ module.exports = {
   devtool: 'inline-source-map',
 
   plugins: [
+    /**
+      * All files inside webpack's output.path directory will be removed once, but the
+      * directory itself will not be. If using webpack 4+'s default configuration,
+      * everything under <PROJECT_DIR>/dist/ will be removed.
+      * Use cleanOnceBeforeBuildPatterns to override this behavior.
+      *
+      * During rebuilds, all webpack assets that are not used anymore
+      * will be removed automatically.
+      */
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       // inject: false,
       hash: true,
@@ -35,14 +45,18 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
 
     // A static asset folder
-    new CopyWebpackPlugin([
-      {
-        from: 'static/**',
-        toType: 'dir',
-        flatten: true,
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: 'static/**',
+          toType: 'dir',
+          flatten: true,
+        },
+      ],
+      options: {
+        concurrency: 100,
       },
-    ],),
-
+    }),
   ],
 
   module: {
