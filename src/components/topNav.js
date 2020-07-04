2d73ba6e7d2a0275082c8e2baf558769ef91a7da
Thijs van Diessen <thijs.vandiessen@incentro.com>
2020-07-04 22:44:22 +0200

    dementions

--- src/components/topNav.js
+++ src/components/topNav.js
@@ -5,7 +5,7 @@ class TopNav extends Component {
   render() {
     return (
       <header className="topNav">
-        <img alt="logo" className="logo" src={logo}/>
+        <img alt="logo" className="logo" src={logo} width="100 " height="100"/>
       </header>
     );
   }
